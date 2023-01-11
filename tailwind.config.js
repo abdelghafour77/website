67e99da4a8b4a42e6b8b07e0ffc5c66a8a4e18a5
abdelghafour aouad <abdelghafour.aouad0@gmail.com>
2023-01-11 19:35:48 +0100

    update file config of tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bluee: '#2d6bad',
+        gold: '#FFD700',
+        goldenrod: '#DAA520',
+        goldenyellow: '#FFDF00',
+        goldtransparent: '#ffd90033',
+        transparentBlack: '#00000066',
+      }
+    },
   },
+
   plugins: [],
 }
